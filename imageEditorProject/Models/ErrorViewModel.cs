78ab6fdfd5dcb79f2d8da6ebc9d98bba7ec87d54
Francisco Rodeño Sanchez <fran_rode@hotmail.com>
2019-04-11 00:16:47 +0200

    Add project files.

---
+++ imageEditorProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace imageEditorProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
